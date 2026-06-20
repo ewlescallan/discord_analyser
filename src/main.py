6c6f150527192b771513d65d6ab74c8141fae8e5
ewlescallan <callan.ewles@protonmail.com>
2026-06-20 21:57:34 +0100

    Completed a basic line chart that updates automatically upon widget update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,71 @@
-import panel
-import matplotlib
+import panel as pn
+import matplotlib.pyplot as plt
+import datetime as dt
 
-if __name__ == '__main__':
-    print("A")
+import reader
+
+
+pn.extension()
+
+messagesArray = reader.setUpMessagesArray()
+startDate, endDate = reader.getStartAndEndDate(messagesArray)
+messagesByDay = reader.getMessagesByDay(messagesArray, startDate, endDate)
+
+def getDayChartVals(messagesByDay):
+    days = list(messagesByDay.keys())
+    vals = []
+    for day in days:
+        #Flatten nested list to get total messages
+        vals.append(len([item for sublist in messagesByDay[day] for item in sublist]))
+    return [days, vals]
+
+def initDayChart(days, vals):
+    fig,ax = plt.subplots(figsize = (4,3))
+    ax.plot(days, vals)
+    plt.close(fig)
+    return fig
+
+
+def updateDayChart(days, vals, startDate, endDate):
+    dayCount = len(days)
+    startGap, endGap = 0, 0
+    if startDate != days[0]:
+        startGap = int(str(startDate - days[0]).split(" ")[0])
+    if endDate != days[-1]:
+        endGap = int(str(days[-1] - endDate).split(" ")[0])
+    days = days[startGap:dayCount - endGap]
+    vals = vals[startGap:dayCount - endGap]
+    fig,ax = plt.subplots(figsize = (4,3))
+    ax.plot(days, vals)
+    plt.close(fig)
+    return fig
+
+
+
+date_range_slider = pn.widgets.DateRangeSlider(
+    label='Date Range Slider',
+    start=startDate, end=endDate,
+    value=(startDate, endDate),
+    step=2
+)
+
+dayChartVals = getDayChartVals(messagesByDay)
+
+
+a = initDayChart(dayChartVals[0], dayChartVals[1])
+dayChart = pn.pane.Matplotlib(a)
+
+def changed(event):
+    print(f"{event.new}")   
+    print(event.new[1] - event.new[0])
+    dayChart.object = updateDayChart(dayChartVals[0], dayChartVals[1], event.new[0], event.new[1])
+    dayChart.param.trigger('object')
+
+date_range_slider.param.watch(changed, 'value')
+
+pn.Column(
+    date_range_slider,
+    dayChart
+).servable()
+
+# initDayChart(messagesByDay, dt.date(2026, 5, 1), endDate)
--- src/reader.py
+++ src/reader.py
@@ -3,13 +3,13 @@ import json
 import datetime
 from message import Message
 
-#Takes a Timestamp from the JSON file and converts it into a datetime date object
+#Takes a timestamp from the JSON file and converts it into a datetime date object
 def convertTimestampToDate(timestamp):
     timestamp = timestamp.split(" ")[0]
     timestamp = timestamp.split("-")
     return datetime.date(year = int(timestamp[0]), month = int(timestamp[1]), day = int(timestamp[2]))
 
-#Takes a Timestamp from the JSON file and converts it into a datetime time object
+#Takes a timestamp from the JSON file and converts it into a datetime time object
 def convertTimestampToTime(timestamp):
     timestamp = timestamp.split(" ")[1]
     timestamp = timestamp.split(":")
@@ -25,25 +25,39 @@ def convertJsonToMessage(messageJson):
         messagesArray.append(Message(date, time, contents))
     return messagesArray
 
-messageFiles = os.listdir("data")
-fileCount = len(messageFiles)
-messages = []
+#Returns the earliest and latest date messages were sent in the channel
+def getStartAndEndDate(messagesArray):
+    startDate = datetime.date(year = 3000, month = 1, day = 1)
+    endDate = datetime.date(year = 1, month = 1, day = 1)
+    for messages in messagesArray:
+        if startDate > messages[-1].date:
+            startDate = messages[-1].date
+        if endDate < messages[0].date:
+            endDate = messages[0].date
+    return startDate, endDate
 
-#Load files into json objects
-for messageFile in messageFiles:
-    with open("data/" + str(messageFile), "r", encoding="utf-8") as f:
-        messages.append(convertJsonToMessage(json.load(f)))
+#Creates the dictionary messagesByDay as such: {<date> : [[<list of messages from user1>], [<list of messages from user2>], ...]}
+def getMessagesByDay(messagesArray, startDate, endDate):
+    messagesByDay = {}
+    tempDate = startDate
+    delta = datetime.timedelta(days=1)
 
-print(messages)
+    #Initialise messagesByDay keys
+    while tempDate <= endDate:
+        messagesByDay[tempDate] = [[] for i in range(len(messagesArray))]
+        tempDate += delta
+    for memberNo in range(len(messagesArray)):
+        for msg in messagesArray[memberNo]:
+            messagesByDay[msg.date][memberNo].append(msg)
+    return messagesByDay
 
-startDate = datetime.date(year = 3000, month = 1, day = 1)
-endDate = datetime.date(year = 1, month = 1, day = 1)
+#Sets up the basic array of Message objects
+def setUpMessagesArray():
+    messageFiles = os.listdir("data")
+    messagesArray = []
 
-
-
-
-
-def getStartAndEndDates(messageJsons):
-    for messageJson in messageJsons:
-        for messages in messageJson:
-            print(messages["Timestamp"])
+    #Load files into json objects
+    for messageFile in messageFiles:
+        with open("data/" + str(messageFile), "r", encoding="utf-8") as f:
+            messagesArray.append(convertJsonToMessage(json.load(f)))
+    return messagesArray
