015555f0544865ba09c038be21090fa46afe9805
ewlescallan <callan.ewles@protonmail.com>
2026-06-22 21:20:57 +0100

    Moved dayChart to own class and added functionality through additional widgets

---
+++ src/dayChartClass.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import panel as pn
+
+class dayChartClass:
+    def __init__(self, messagesByDay, startDate, endDate, participantCount):
+        self.messagesByDay = messagesByDay
+        self.days = list(messagesByDay.keys())
+        self.dayCount = len(self.days)
+        self.valsTotal, self.valsInd = self.getVals()
+        self.chart = self.initDayChart()
+        self.pane = pn.pane.Matplotlib(self.chart)
+        self.daySlider = self.initDaySlider(startDate, endDate)
+        self.axisLockBox = pn.widgets.Checkbox(label="Lock Axis")
+        self.linesBoxes = self.initLinesBoxes(participantCount)
+
+    def getVals(self):
+        valsTotal = []
+        valsInd = [[] for i in range(len(self.messagesByDay[self.days[0]]))]
+        for day in self.days:
+            #Flatten nested list to get total messages
+            valsTotal.append(len([item for sublist in self.messagesByDay[day] for item in sublist]))
+            for memberNo in range(len(valsInd)):
+                valsInd[memberNo].append(len(self.messagesByDay[day][memberNo]))
+        return valsTotal, valsInd
+    
+    def initDaySlider(self, startDate, endDate):
+        return pn.widgets.DateRangeSlider(
+            label='Date Range Slider',
+            start=startDate, end=endDate,
+            value=(startDate, endDate),
+            step=2
+        )
+
+    def initLinesBoxes(self, participantCount):
+        dayChartLines = [pn.widgets.Checkbox(label="Total")]
+        dayChartLines[0].value = True
+        for memberNo in range(participantCount):
+            dayChartLines.append(pn.widgets.Checkbox(label=f"Participant {memberNo + 1}"))
+        return dayChartLines
+
+    #Create initial daily message chart
+    def initDayChart(self):
+        fig,ax = plt.subplots(figsize = (4,3))
+        ax.plot(self.days, self.valsTotal)
+        plt.close(fig)
+        return fig
+
+    #Update daily message chart, taking widgets into account
+    def updateDayChart(self, startDate, endDate):
+        maxValue = 0
+        startGap, endGap = self.getDayChartGap(startDate, endDate)
+        days = self.days[startGap : self.dayCount - endGap]
+        fig,ax = plt.subplots(figsize = (4,3))
+        if self.linesBoxes[0].value:
+            maxValue = max(maxValue, max(self.valsTotal))
+            valsTotal = self.valsTotal[startGap : self.dayCount - endGap]
+            ax.plot(days, valsTotal)
+        for dayChartLineNo in range(1, len(self.linesBoxes)):
+            if self.linesBoxes[dayChartLineNo].value:
+                maxValue = max(maxValue, max(self.valsInd[dayChartLineNo - 1]))
+                indLine = self.valsInd[dayChartLineNo - 1][startGap : self.dayCount - endGap]
+                ax.plot(days, indLine)
+        if self.axisLockBox.value:
+            ax.set_ylim(top=maxValue + 100)
+        plt.close(fig)
+        return fig
+
+    def getDayChartGap(self, startDate, endDate):
+        startGap, endGap = 0, 0
+        firstDay = self.days[0]
+        lastDay = self.days[-1]
+        if startDate != firstDay:
+            startGap = int(str(startDate - firstDay).split(" ")[0])
+        if endDate != lastDay:
+            endGap = int(str(lastDay - endDate).split(" ")[0])
+        return startGap, endGap
+    
+    def chartChanged(self, event):
+        self.pane.object = self.updateDayChart(self.daySlider.value_start, self.daySlider.value_end)
+        self.pane.param.trigger('object')
--- src/main.py
+++ src/main.py
@@ -2,70 +2,33 @@ import panel as pn
 import matplotlib.pyplot as plt
 import datetime as dt
 
+from dayChartClass import dayChartClass
 import reader
 
 
 pn.extension()
 
 messagesArray = reader.setUpMessagesArray()
+participantCount = len(messagesArray)
 startDate, endDate = reader.getStartAndEndDate(messagesArray)
 messagesByDay = reader.getMessagesByDay(messagesArray, startDate, endDate)
 
-def getDayChartVals(messagesByDay):
-    days = list(messagesByDay.keys())
-    vals = []
-    for day in days:
-        #Flatten nested list to get total messages
-        vals.append(len([item for sublist in messagesByDay[day] for item in sublist]))
-    return [days, vals]
+dayChart = dayChartClass(messagesByDay, startDate, endDate, participantCount)
 
-def initDayChart(days, vals):
-    fig,ax = plt.subplots(figsize = (4,3))
-    ax.plot(days, vals)
-    plt.close(fig)
-    return fig
-
-
-def updateDayChart(days, vals, startDate, endDate):
-    dayCount = len(days)
-    startGap, endGap = 0, 0
-    if startDate != days[0]:
-        startGap = int(str(startDate - days[0]).split(" ")[0])
-    if endDate != days[-1]:
-        endGap = int(str(days[-1] - endDate).split(" ")[0])
-    days = days[startGap:dayCount - endGap]
-    vals = vals[startGap:dayCount - endGap]
-    fig,ax = plt.subplots(figsize = (4,3))
-    ax.plot(days, vals)
-    plt.close(fig)
-    return fig
-
-
-
-date_range_slider = pn.widgets.DateRangeSlider(
-    label='Date Range Slider',
-    start=startDate, end=endDate,
-    value=(startDate, endDate),
-    step=2
-)
-
-dayChartVals = getDayChartVals(messagesByDay)
-
-
-a = initDayChart(dayChartVals[0], dayChartVals[1])
-dayChart = pn.pane.Matplotlib(a)
-
-def changed(event):
-    print(f"{event.new}")   
-    print(event.new[1] - event.new[0])
-    dayChart.object = updateDayChart(dayChartVals[0], dayChartVals[1], event.new[0], event.new[1])
-    dayChart.param.trigger('object')
-
-date_range_slider.param.watch(changed, 'value')
+dayChart.daySlider.param.watch(dayChart.chartChanged, 'value')
+dayChart.axisLockBox.param.watch(dayChart.chartChanged, 'value')
+for lineCheckBox in dayChart.linesBoxes:
+    lineCheckBox.param.watch(dayChart.chartChanged, 'value')
 
 pn.Column(
-    date_range_slider,
-    dayChart
+    pn.Row(
+        dayChart.daySlider,
+        dayChart.axisLockBox
+    ),
+    pn.Row(
+        pn.Column(*dayChart.linesBoxes),
+        dayChart.pane
+    )
 ).servable()
 
-# initDayChart(messagesByDay, dt.date(2026, 5, 1), endDate)
+
